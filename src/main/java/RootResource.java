13ba3ef5da68fe162e13281c66b2fa95e44f6d2f
Livre programacao ltda-me <livre.programacao@gmail.com>
2017-02-02 17:56:45 -0200

    Updated RootResource class

--- src/main/java/RootResource.java
+++ src/main/java/RootResource.java
@@ -1,8 +1,8 @@
 
 import java.util.List;
-import javax.enterprise.inject.Produces;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 /*
